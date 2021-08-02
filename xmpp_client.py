9020c4a8e35d1e52e527e9b9f116d7a78ceeb556
Douglas De León <douglasdeleon@Douglass-MacBook-Pro.local>
2021-08-02 17:38:15 -0600

    ADD: EchoBot example

---
+++ xmpp_client.py
@@ -0,0 +1,47 @@
+
+import asyncio
+import logging
+
+from slixmpp import ClientXMPP
+
+
+class EchoBot(ClientXMPP):
+
+    def __init__(self, jid, password):
+        ClientXMPP.__init__(self, jid, password)
+
+        self.add_event_handler("session_start", self.session_start)
+        self.add_event_handler("message", self.message)
+
+        # If you wanted more functionality, here's how to register plugins:
+        # self.register_plugin('xep_0030') # Service Discovery
+        # self.register_plugin('xep_0199') # XMPP Ping
+
+        # Here's how to access plugins once you've registered them:
+        # self['xep_0030'].add_feature('echo_demo')
+
+    def session_start(self, event):
+        self.send_presence()
+        self.get_roster()
+
+        # Most get_*/set_* methods from plugins use Iq stanzas, which
+        # are sent asynchronously. You can almost always provide a
+        # callback that will be executed when the reply is received.
+
+    def message(self, msg):
+        if msg['type'] in ('chat', 'normal'):
+            msg.reply("Thanks for sending\n%(body)s" % msg).send()
+
+
+if __name__ == '__main__':
+    # Ideally use optparse or argparse to get JID,
+    # password, and log level.
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(levelname)-8s %(message)s'
+    )
+
+    xmpp = EchoBot('somejid@example.com', 'use_getpass')
+    xmpp.connect()
+    xmpp.process()
